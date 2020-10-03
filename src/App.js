b276796442579a3aba80c16febb0608b44187465
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-03 17:16:57 +0530

    add Header Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './Header';
 
 // Statics
 import './App.css';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="app">
       {/* Header */}
+      <Header />
       {/* Aboutme */}
       {/* Projects */}
       {/* Footer */}
