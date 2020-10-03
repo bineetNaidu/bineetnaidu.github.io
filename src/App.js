099b717a3ba1bea1d707c5b2e3b118f0078e10da
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-03 16:32:12 +0530

    -- setting up W/ reactJS -- add dependencies

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Statics
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      {/* Aboutme */}
+      {/* Projects */}
+      {/* Footer */}
+    </div>
+  );
+}
+
+export default App;
