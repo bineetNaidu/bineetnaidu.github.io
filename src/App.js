caa2c2a05f0645f5ee6a77a6eac3ac77e8d7c822
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-04 11:55:37 +0530

    add particle JS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ProjectLists from './ProjectLists';
 import Footer from './Footer';
 import TechStacks from './TechStacks';
 import { motion } from 'framer-motion';
+import Particles from 'react-particles-js';
 
 // Statics
 import './App.css';
@@ -12,6 +13,20 @@ import './App.css';
 function App() {
   return (
     <motion.div layout className="app">
+      <Particles
+        style={{ position: 'absolute', zIndex: '-100' }}
+        params={{
+          particles: {
+            number: {
+              value: 100,
+              density: {
+                enable: false,
+                value_area: 1000,
+              },
+            },
+          },
+        }}
+      />
       <Header />
       <AboutMe />
       <TechStacks />
