ae8d21ae4b024783ac1736549041bf923a97bf95
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-24 00:34:11 +0530

    -- refactor -- add projects data -- cleaned Projects List Component

--- src/ProjectLists.jsx
+++ src/ProjectLists.jsx
@@ -1,18 +1,10 @@
 import React, { memo } from 'react';
 import Project from './Project';
 import { motion } from 'framer-motion';
+import { projects } from "./data/projects.data";
 
 // Statics
 import './ProjectLists.css';
-import covid19 from './images/covid19.png';
-import fb from './images/fb.png';
-import firegram from './images/firegram.png';
-import hulu from './images/hulu.png';
-import instagram from './images/instagram.png';
-import messenger from './images/messenger.png';
-import netflixClone from './images/netflixClone.png';
-import uiColors from './images/uiColors.png';
-import yelpcamp from './images/yelpcamp.png';
 
 // Variants
 
@@ -45,60 +37,16 @@ const ProjectLists = () => {
         layout
         className="projectsLists"
       >
-        <Project
-          image={netflixClone}
-          github="https://github.com/bineetNaidu/netflix-clone"
-          name="Netflix Clone"
-          url="https://bineet-netflix-clone.netlify.app/"
-        />
-        <Project
-          image={uiColors}
-          url="https://ui-colors.netlify.app/"
-          github="https://github.com/bineetNaidu/UI-Colors-App"
-          name="UI Colors"
-        />
-        <Project
-          image={covid19}
-          url="https://bineet-covid19-tracker.netlify.app/"
-          github="https://github.com/bineetNaidu/Covid19-Tracker"
-          name="Covid19 Tracker"
-        />
-        <Project
-          image={fb}
-          url="https://facebook-clone-bd13e.web.app/"
-          github="https://github.com/bineetNaidu/facebook-clone"
-          name="Facebook Clone"
-        />
-        <Project
-          image={hulu}
-          url="https://bineet-hulu-clone.netlify.app/"
-          name="Hulu Clone"
-          github="https://github.com/bineetNaidu/hulu-clone"
-        />
-        <Project
-          image={instagram}
-          url="https://bineet-instagram-clone.web.app/"
-          name="Instagram Clone"
-          github="https://github.com/bineetNaidu/instagram-clone"
-        />
-        <Project
-          image={firegram}
-          url="https://firegram-82050.web.app/"
-          github="https://github.com/bineetNaidu/firegram"
-          name="Firegram"
-        />
-        <Project
-          image={messenger}
-          url="https://messenger-clone-edd60.web.app/"
-          github="https://github.com/bineetNaidu/messenger-clone"
-          name="Messenger Clone"
-        />
-        <Project
-          image={yelpcamp}
-          url="https://bineetsyelpcamp.herokuapp.com/"
-          name="YelpCamp"
-          github="https://github.com/bineetNaidu/YelpCamp"
-        />
+        {projects.map(p => (
+          <Project
+            key={p.name}
+          image={p.image}
+          github={p.github}
+          name={p.name}
+          url={p.url}
+          />
+          ))}
+      
       </motion.div>
     </>
   );
