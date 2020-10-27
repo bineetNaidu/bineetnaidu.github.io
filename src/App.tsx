aecd75998ac3594d70b972c9cd1ef26094bfb65d
bineetNaidu <bineet.s.naidu@gmail.com>
2020-10-28 00:42:41 +0530

    add Header TS component w/ styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Header';
+import { motion } from 'framer-motion';
+import { useStateValue } from './context/State.context';
+// Statics
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  // Contexts
+  const [{ body }] = useStateValue();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div layout className="app" style={{ background: body }}>
+      <Header />
+    </motion.div>
   );
-}
+};
 
 export default App;
