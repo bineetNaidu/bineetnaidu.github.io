38d49d3046d47837edb0a691ab6403b257985cf3
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-19 23:21:52 +0530

    add framer motion base animations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,23 +7,85 @@ import circleSvg from '../assets/circle.svg';
 import lineSvg from '../assets/line-bars.svg';
 import './Home.css';
 
+const containerVariants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+};
+
+const wrapperVariant = {
+  initial: {
+    y: '4vh',
+  },
+  animate: {
+    y: 0,
+    transition: { delay: 0.1 },
+  },
+};
+
+const brandVariant = {
+  initial: {
+    opacity: 0,
+    y: '-10vh',
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: { delay: 0.3 },
+  },
+};
+
+const navigationVariant1 = {
+  initial: {
+    opacity: 0,
+    x: '-10vw',
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: { delay: 0.3 },
+  },
+};
+
+const navigationVariant2 = {
+  initial: {
+    opacity: 0,
+    x: '10vw',
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: { delay: 0.3 },
+  },
+};
+
 const Home: React.FC = () => {
   return (
-    <motion.div className="home">
-      <div className="home__container">
-        <div className="home__navigation1">
+    <motion.div
+      className="home"
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+    >
+      <motion.div className="home__container" variants={wrapperVariant}>
+        <motion.div variants={navigationVariant1} className="home__navigation1">
           <NavigationBar label="About" link="/about" />
-        </div>
-        <div className="home__navigation2">
+        </motion.div>
+        <motion.div variants={navigationVariant2} className="home__navigation2">
           <NavigationBar label="Works" link="/works" direction="invert" />
-        </div>
+        </motion.div>
 
-        <h1 className="home__brand">Bineet Naidu</h1>
+        <motion.h1 variants={brandVariant} className="home__brand">
+          Bineet Naidu
+        </motion.h1>
         <div className="home__images">
           <img src={circleSvg} alt="circle svg" width="100%" height="100%" />
           <img src={lineSvg} alt="line bar svg" width="100%" height="100%" />
         </div>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
