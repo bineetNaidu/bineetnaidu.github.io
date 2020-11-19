6c29712e3b830ad47da21c758b40604d5e56b7d2
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-19 18:48:53 +0530

    add base Home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import NavigationBar from '../components/NavigationBar';
+
+// Statics
+import circleSvg from '../assets/circle.svg';
+import lineSvg from '../assets/line-bars.svg';
+import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <motion.div className="home">
+      <div className="home__container">
+        <div className="home__navigation1">
+          <NavigationBar label="About" link="/about" />
+        </div>
+        <div className="home__navigation2">
+          <NavigationBar label="Works" link="/works" direction="invert" />
+        </div>
+
+        <h1 className="home__brand">Bineet Naidu</h1>
+        <div className="home__images">
+          <img src={circleSvg} alt="circle svg" width="100%" height="100%" />
+          <img src={lineSvg} alt="line bar svg" width="100%" height="100%" />
+        </div>
+      </div>
+    </motion.div>
   );
 };
 
