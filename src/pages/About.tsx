281ea187de82607cca8f2d1573ece23f1716293f
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-19 22:01:20 +0530

    add base About page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// Statics
+import './About.css';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="about">
+      <div className="about__header">
+        <Link to="/" className="about__header--brand">
+          Bineet Naidu
+        </Link>
+        <Link to="/works" className="about__header--workLink">
+          Works
+        </Link>
+      </div>
+
+      <div className="about__content">
+        <span className="about__tileMusk">Forget safety,</span>
+        <span className="about__tileMusk">be notorious.</span>
+        <span className="about__tileMusk">You Break Stuff,</span>
+        <span className="about__tileMusk">And I make stuff</span>
+      </div>
+
+      <div className="about__me">
+        <p>
+          I build new projects just to tickle my brain. I Love to help and
+          Contribute to Open source Projects...
+        </p>
+      </div>
     </div>
   );
 };
