716caeeab93a8e8557583217ac9e497637dfc522
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-22 17:40:00 +0530

    add home social links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import NavigationBar from '../components/NavigationBar';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import IconButton from '@material-ui/core/IconButton';
 import {
   containerVariants,
   wrapperVariant,
@@ -36,6 +40,20 @@ const Home: React.FC = () => {
         <div className="home__images">
           <img src={circleSvg} alt="circle svg" width="100%" height="100%" />
           <img src={lineSvg} alt="line bar svg" width="100%" height="100%" />
+        </div>
+        <div className="home__ctx">
+          <div className="home__socials">
+            <IconButton color="secondary">
+              <GitHubIcon />
+            </IconButton>
+            <IconButton>
+              <InstagramIcon color="secondary" />
+            </IconButton>
+            <IconButton>
+              <TwitterIcon color="secondary" />
+            </IconButton>
+          </div>
+          <h5>EN</h5>
         </div>
       </motion.div>
     </motion.div>
