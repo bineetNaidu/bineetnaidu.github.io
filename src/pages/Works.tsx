2f72337725610971b6ae8a6cd5c8315c12caedde
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-22 22:14:12 +0530

    setting up works page

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import ProjectTile from '../components/ProjectTile';
+import useFirestore from '../hooks/useFirestore';
+// Statics
+import './Works.css';
+interface ProjectInterfaces {
+  id: string;
+  name: string;
+  url: string;
+  link: string;
+  github: string;
+}
 
 const Works: React.FC = () => {
+  const { docs } = useFirestore('projects');
   return (
-    <div>
-      <h1>My Works</h1>
+    <div className="works">
+      {docs.length > 0 &&
+        docs.map((p: ProjectInterfaces) => (
+          <ProjectTile
+            description="Deign + UI + ux"
+            idx={1}
+            name={p.name}
+            link={p.link}
+            image={p.url}
+          />
+        ))}
     </div>
   );
 };
