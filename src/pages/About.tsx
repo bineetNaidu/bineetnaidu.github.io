8e6c8ef7545ef0946a32e559d47a3db21ccf2ec6
bineetNaidu <bineet.s.naidu@gmail.com>
2020-11-30 23:53:24 +0530

    add techs lists

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,13 +5,17 @@ import {
   aboutMeVariant,
   backdropVariant,
   tilemuskVariant,
+  imgVariants as techVariant,
 } from '../variants/About.variants';
+import Header from '../components/Header';
+import useFirestore from '../hooks/useFirestore';
+import { Techs } from '../types';
 
 // Statics
 import './About.css';
-import Header from '../components/Header';
 
 const About: React.FC = () => {
+  const { docs } = useFirestore('techs');
   return (
     <motion.div
       variants={containerVariant}
@@ -21,27 +25,47 @@ const About: React.FC = () => {
     >
       <motion.div className="backdrop" variants={backdropVariant}></motion.div>
       <Header pageLinks="/works" page="Works" />
-      <div className="about__content">
-        <motion.span variants={tilemuskVariant} className="about__tileMusk">
-          Forget safety,
-        </motion.span>
-        <motion.span variants={tilemuskVariant} className="about__tileMusk">
-          be notorious.
-        </motion.span>
-        <motion.span variants={tilemuskVariant} className="about__tileMusk">
-          You Break Stuff,
-        </motion.span>
-        <motion.span variants={tilemuskVariant} className="about__tileMusk">
-          And I make stuff
-        </motion.span>
+      <div>
+        <div className="about__content">
+          <motion.span variants={tilemuskVariant} className="about__tileMusk">
+            Forget safety,
+          </motion.span>
+          <motion.span variants={tilemuskVariant} className="about__tileMusk">
+            be notorious.
+          </motion.span>
+          <motion.span variants={tilemuskVariant} className="about__tileMusk">
+            You Break Stuff,
+          </motion.span>
+          <motion.span variants={tilemuskVariant} className="about__tileMusk">
+            And I make stuff
+          </motion.span>
+        </div>
+
+        <motion.div variants={aboutMeVariant} className="about__me">
+          <p>
+            I build new projects just to tickle my brain. I Love to help and
+            Contribute to Open source Projects...
+          </p>
+        </motion.div>
       </div>
 
-      <motion.div variants={aboutMeVariant} className="about__me">
-        <p>
-          I build new projects just to tickle my brain. I Love to help and
-          Contribute to Open source Projects...
-        </p>
-      </motion.div>
+      <div className="about__techs">
+        {docs.map((t: Techs) => (
+          <motion.div
+            className="about__techItem"
+            variants={techVariant}
+            key={t.id}
+          >
+            <img
+              className="about__techImg"
+              alt={t.name}
+              title={t.name}
+              src={t.image}
+            />
+            <h4>{t.name}</h4>
+          </motion.div>
+        ))}
+      </div>
     </motion.div>
   );
 };
--- src/variants/About.variants.ts
+++ src/variants/About.variants.ts
@@ -38,3 +38,20 @@ export const aboutMeVariant = {
     transition: { delay: 1.6, duration: 0.8 },
   },
 };
+
+export const imgVariants = {
+  initial: {
+    x: '-100vh',
+    opacity: 0,
+  },
+
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      when: 'beforeChildren',
+      staggerChildren: 0.4,
+    },
+  },
+};
