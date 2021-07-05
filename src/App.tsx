e4494b208159ed609314ee1d963a80fa7b5ca810
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-05 22:32:48 +0530

    🔥 add SASS support!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import './App.css';
+import './styles/App.scss';
 
 const App: FC = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
 // Statics
-import './index.css';
+import './styles/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
