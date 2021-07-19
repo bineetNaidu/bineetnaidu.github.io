76e12a61a5aa887c7cc668e2a6e86503df32d2f3
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-19 12:08:13 +0530

    🚀 add social Handlers

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import '../styles/Wrapper.scss';
+import { SocialHandlers } from './SocialHandlers';
 
 export const Wrapper: FC = ({ children }) => {
   return (
     <div className="wrapper">
       {/* Dangling Social Links */}
+      <SocialHandlers />
       <div className="wrapper__body">{children}</div>
       {/* Dangling Mail Link */}
     </div>
