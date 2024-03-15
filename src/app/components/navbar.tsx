2c3f4be49ba97dcea090fab0501b7fc4df1679c8
Bineet Naidu <bineet.s.naidu@gmail.com>
2024-03-15 19:59:59 +0530

    🚀 updated nav

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -45,7 +45,7 @@ export function Navbar() {
               </div>
             </a>
           </li>
-          <li>
+          {/* <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="#blogs"
@@ -54,7 +54,7 @@ export function Navbar() {
                 BLOGS
               </div>
             </a>
-          </li>
+          </li> */}
           <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
