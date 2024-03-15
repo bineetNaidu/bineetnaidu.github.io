14ab8dd140eb7614f953280bbbf3d17cf906c681
Bineet Naidu <bineet.s.naidu@gmail.com>
2024-03-15 18:54:56 +0530

    ✨ initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { HeroSection } from './components/homepage/HeroSection';
+
+export default async function Home() {
+  return (
+    <>
+      <HeroSection />
+      {/* <AboutSection />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Education />
+      <Blog blogs={blogs} />
+      <ContactSection /> */}
+    </>
+  );
+}
---
+++ src/app/utils/data/personal-data.ts
@@ -0,0 +1,15 @@
+export const personalData = {
+  name: 'Bineet Naidu',
+  profile: '/profile.png',
+  designation: 'Software Developer',
+  description:
+    "I'm a FullStack Javascript developer who specializes in building (and occasionally designing) exceptional digital experiences. Currently, I'm an helping open source project to get better and reach there goals.",
+  email: 'bineetnaiduapps@gmail.com',
+  phone: '+8801608797655',
+  address: 'Bangalore, Karnataka, India',
+  github: 'https://github.com/bineetNaidu',
+  linkedIn: 'https://www.linkedin.com/in/bineetNaidu',
+  twitter: 'https://twitter.com/bineet_naidu',
+  resume:
+    'https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing',
+};
