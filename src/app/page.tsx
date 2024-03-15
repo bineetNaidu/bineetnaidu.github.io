028b47451ae327283a5b40a824ba427d0c86ad11
Bineet Naidu <bineet.s.naidu@gmail.com>
2024-03-15 19:43:59 +0530

    🚀 add Experience Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { HeroSection } from './components/homepage/HeroSection';
 import { AboutSection } from './components/homepage/about';
+import Experience from './components/homepage/experience';
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
       <AboutSection />
-      {/* <Experience />
-      <Skills />
+      <Experience />
+      {/* <Skills />
       <Projects />
       <Education />
       <Blog blogs={blogs} />
