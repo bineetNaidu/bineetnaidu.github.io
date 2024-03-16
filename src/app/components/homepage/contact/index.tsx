d42dcfd45bf48388a0330753d017fb0f1cc69924
Bineet Naidu <bineet.s.naidu@gmail.com>
2024-03-16 16:45:50 +0530

    🚀 update contact page

--- src/app/components/homepage/contact/index.tsx
+++ src/app/components/homepage/contact/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { BiLogoLinkedin } from 'react-icons/bi';
 import { CiLocationOn } from 'react-icons/ci';
 import { FaXTwitter } from 'react-icons/fa6';
-import { IoLogoGithub, IoMdCall } from 'react-icons/io';
+import { IoLogoGithub } from 'react-icons/io';
 import { MdAlternateEmail } from 'react-icons/md';
 import { personalData } from '@/app/utils/data/personal-data';
 import { ContactForm } from './contact-form';
@@ -29,13 +29,7 @@ export function ContactSection() {
               />
               <span>{personalData.email}</span>
             </p>
-            <p className="text-sm md:text-xl flex items-center gap-3">
-              <IoMdCall
-                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
-                size={36}
-              />
-              <span>{personalData.phone}</span>
-            </p>
+
             <p className="text-sm md:text-xl flex items-center gap-3">
               <CiLocationOn
                 className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
